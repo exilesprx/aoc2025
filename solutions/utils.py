ab38495fc280a36b9fc6d1f782f4ca5ee59740ff
Exiles.prx <exiles.prx@gmail.com>
2025-12-03 20:14:43 -0500

    fix: read input

--- solutions/utils.py
+++ solutions/utils.py
@@ -1,3 +1,10 @@
-def read_input(file_path):
-    with open(file_path, "r") as file:
-        return [line.strip() for line in file]
+from typing import List
+
+
+def read_input(file_path: str) -> List[str]:
+    try:
+        with open(file_path, "r") as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        print(f"Error: The file at {file_path} was not found.")
+        return []
