44baf1adc26e574a79e2b2716c936d93c190f336
Exiles.prx <exiles.prx@gmail.com>
2025-12-06 16:56:37 -0500

    feat: solved day 1 part 2

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,8 +1,9 @@
 import sys
 from solutions.utils import read_input
+from typing import Tuple
 
 
-def solve(file_path: str, starting_point: int = 50) -> int:
+def part1(file_path: str, starting_point: int = 50) -> int:
     data = read_input(file_path)
     password = 0
 
@@ -28,8 +29,48 @@ def solve(file_path: str, starting_point: int = 50) -> int:
     return password
 
 
+def part2(file_path: str, starting_point: int = 50) -> int:
+    data = read_input(file_path)
+    password = 0
+    started_at_zero = starting_point == 0
+
+    for line in data:
+        parts = [line[0], line[1:]]
+        direction = parts[0]
+        distance = int(parts[1]) % 100  # remove full turns and get the remainder
+        full_turns = int(parts[1]) // 100  # add times pointed at 0 for full turns
+
+        if direction == "L":
+            starting_point -= distance
+            if starting_point < 0:
+                starting_point += 100
+                password += 1 if not started_at_zero and starting_point != 0 else 0
+        elif direction == "R":
+            starting_point += distance
+            if starting_point > 99:
+                starting_point -= 100
+                password += 1 if not started_at_zero and starting_point != 0 else 0
+        else:
+            raise ValueError(f"Invalid direction: {direction}")
+
+        if starting_point == 0:
+            password += full_turns + 1
+            started_at_zero = True
+        else:
+            password += full_turns
+            started_at_zero = False
+
+    return password
+
+
+def solve(file_path: str, starting_point: int = 50) -> Tuple[int, int]:
+    return part1(file_path, starting_point), part2(file_path, starting_point)
+
+
 if __name__ == "__main__":
     args = sys.argv
     if len(args) < 2:
         exit("Please provide the input file path as a command-line argument.")
-    solve(sys.argv[1])
+    result = solve(sys.argv[1])
+    print(f"Part 1: {result[0]}")
+    print(f"Part 2: {result[1]}")
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,13 +1,13 @@
-from solutions.day01 import solve
+from solutions.day01 import part1, part2, solve
 
 
-def test_example_file():
+def test_example_file_part_1():
     file = "data/day01.example.txt"
-    result = solve(file)
+    result = part1(file)
     assert result == 3
 
 
-def test_input_file():
-    file = "data/day01.txt"
-    result = solve(file)
-    assert result == 1
+def test_example_file_part_2():
+    file = "data/day01.example2.txt"
+    result = part2(file)
+    assert result == 6
